2366253bf95538f9413bd9b0761022a41df5a2e3
Ashley <64813439+BintAmun@users.noreply.github.com>
2023-01-22 20:54:08 -0500

    Math Operators

---
+++ Variables.java
@@ -0,0 +1,10 @@
+
+class Variables{
+    public static void main(String[] args) {
+        int passengers = 1;
+        System.out.println(passengers);
+
+        int busTickets = passengers;
+        System.out.println(busTickets);
+    }
+}
